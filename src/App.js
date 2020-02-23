c252937a0061ccfa746ebb744bce21e566442c8a
tmplcat <tmplcat@gmail.com>
2020-02-24 00:22:35 +0900

    画面表示

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            tasks: [
+                {
+                    id: 1,
+                    body: 'とりま表示'
+                },
+                {
+                    id: 2,
+                    body: 'いまだけ...'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="taxks">
+                    {
+                        this.state.tasks.map(task => {
+                            return <div className="task" key={task.id}>{task.body}</div>
+                        })
+                    }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
