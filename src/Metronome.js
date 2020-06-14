810c2136bc13e7e9dabdbbbb8d9898f9ce287979
tmplcat <tmplcat@gmail.com>
2020-06-15 04:08:52 +0900

    一定時間ごとのクリック音の再生と停止ができるように

--- src/Metronome.js
+++ src/Metronome.js
@@ -2,17 +2,44 @@ import React, {Component} from 'react';
 import './App.css';
 
 class Metronome extends Component {
-  play = () => (
+  constructor(props) {
+    super(props);
+    this.state = {
+      strongBeatSound: '/sounds/Claves.wav',
+      weekBeatSound: '/sounds/Rim_Shot.wav',
+      bpm: 120,
+      beatPerClick: 1,
+      volume: 1.0,
+    };
+
+    this.play = this.play.bind(this);
+    this.stop = this.stop.bind(this);
+  }
+
+  sound() {
     document.getElementById('sound-file').play()
-  )
+  }
+
+  play() {
+    let intervalId = setInterval(() => {
+      this.sound();
+    }, 1000);
+
+    this.setState({intervalId: intervalId})
+  }
+
+  stop() {
+    clearInterval(this.state.intervalId);
+  }
 
   render() {
     return (
       <div>
         <h3>Metronome</h3>
-        <button onClick={this.play}>submit</button>
+        <button onClick={this.play}>start</button>
+        <button onClick={this.stop}>stop</button>
         <audio id="sound-file" preload="auto">
-          <source src="/sounds/Claves.wav" type="audio/wav"/>
+          <source src={this.state.strongBeatSound} type="audio/wav"/>
         </audio>
       </div>
     )
