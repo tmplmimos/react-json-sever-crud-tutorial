52feb9e63cb18558a4bb611516133f3cfa9c54f6
tmplcat <tmplcat@gmail.com>
2020-06-15 04:33:50 +0900

    一応 bpm を変えられるように

--- src/Metronome.js
+++ src/Metronome.js
@@ -7,13 +7,14 @@ class Metronome extends Component {
     this.state = {
       strongBeatSound: '/sounds/Claves.wav',
       weekBeatSound: '/sounds/Rim_Shot.wav',
-      bpm: 120,
+      bpm: 100,
       beatPerClick: 1,
       volume: 1.0,
     };
 
     this.play = this.play.bind(this);
     this.stop = this.stop.bind(this);
+    this.setBpm = this.setBpm.bind(this);
   }
 
   sound() {
@@ -23,7 +24,7 @@ class Metronome extends Component {
   play() {
     let intervalId = setInterval(() => {
       this.sound();
-    }, 1000);
+    }, 60000 / this.state.bpm);
 
     this.setState({intervalId: intervalId})
   }
@@ -32,10 +33,15 @@ class Metronome extends Component {
     clearInterval(this.state.intervalId);
   }
 
+  setBpm(event) {
+    this.setState({bpm: event.target.value});
+  }
+
   render() {
     return (
       <div>
         <h3>Metronome</h3>
+        <input type="number" onChange={this.setBpm} value={this.state.bpm}/>
         <button onClick={this.play}>start</button>
         <button onClick={this.stop}>stop</button>
         <audio id="sound-file" preload="auto">
