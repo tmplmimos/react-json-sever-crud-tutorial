83f286d25389f977244caba6f7ac04c1779e78a0
tmplcat <tmplcat@gmail.com>
2020-06-17 03:20:39 +0900

    react-transition-group を導入(仮)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 import Links from './Links';
 import Tasks from './Tasks';
 import Metronome from './Metronome';
 import Home from './Home';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <div>
-            <Links /><hr/>
-            <Route exact path='/' component={Home}/>
-            <Route path='/Tasks' component={Tasks}/>
-            <Route path='/Metronome' component={Metronome}/>
-          </div>
-        </Router>
-      </div>
-    );
-  }
+const defaultStyle = {
+  transition: `opacity 300ms ease-in-out`,
+}
+
+const ROUTES = [
+  { name: 'Home', path: '/', Component: Home },
+  { name: 'Tasks', path: '/Tasks', Component: Tasks },
+  { name: 'Metronome', path: '/Metronome', Component: Metronome }
+];
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Links /><hr/>
+          {ROUTES.map(({path, Component}) => (
+            <Route key={path} path={path} exact>
+              {({ match}) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames='fade'
+                  unmountOnExit
+                >
+                  <div style={{...defaultStyle}}>
+                    <Component/>
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
